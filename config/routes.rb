656954b6f7f4de420d1dc79b226471ae8c4b7a5b
zewdu erkyhun <zewdu444@gmail.com>
2023-05-22 16:42:01 +0300

    Add name to sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:index] do
-
+  resources :users, only: [:index, :show] do
   end
 
   devise_scope :user do
