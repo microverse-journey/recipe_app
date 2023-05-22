c6da06240a19a7ba598f84810a85966b3bb05143
zewdu erkyhun <zewdu444@gmail.com>
2023-05-22 19:22:52 +0300

    Add recipe model

---
+++ db/migrate/20230522160345_create_recipe.rb
@@ -0,0 +1,13 @@
+class CreateRecipe < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.float :preparation_time
+      t.float :cooking_time
+      t.text :description
+      t.public:boolean , default:false
+      t.references :users, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
