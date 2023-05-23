5b256b81f51c1a10c379312e374679a979e2c7fd
zewdu erkyhun <zewdu444@gmail.com>
2023-05-23 18:13:02 +0300

    Fix linter error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,15 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
+
   def new
     @recipe = current_user.recipes.new
   end
+
   def create
     @recipe = current_user.recipes.new(recipe_params)
     if @recipe.save
@@ -20,17 +23,17 @@ class RecipesController < ApplicationController
 
   def destroy
     @recipe = current_user.recipes.find(params[:id])
-    @recipe.destroy if @recipe
+    @recipe&.destroy
     redirect_to recipes_path
   end
 
- def public_recipes
+  def public_recipes
     @recipes = Recipe.where(public: true).order(:name)
- end
+  end
 
- private
+  private
+
   def recipe_params
     params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
   end
-
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
- belongs_to :user
- validates :name, presence: true
+  belongs_to :user
+  validates :name, presence: true
 end
