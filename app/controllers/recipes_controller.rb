4266f77f9927ce626c5ff4f96970b2d4940bf17b
zewdu erkyhun <zewdu444@gmail.com>
2023-05-23 14:01:01 +0300

    Add create form for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,26 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+  def new
+    @recipe = current_user.recipes.new
+  end
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.destroy if @recipe
+    redirect_to recipes_path
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
+  end
+
 end
