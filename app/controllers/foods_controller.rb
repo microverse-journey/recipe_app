135d8ba0fb6a893ef6a872bffbc338046215c41c
mrprotocoll <lekanvgbg@gmail.com>
2023-05-23 13:39:52 +0100

    Add create action for foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,4 +5,15 @@ class FoodsController < ApplicationController
     def new
         @food = Food.new
     end
+
+    def create
+        render :new unless current_user.foods.create(food_params)
+        
+        redirect_to user_foods_path(current_user), notice: "Food successfully created"
+    end
+
+    private
+    def food_params
+        params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
+    end
 end
