3bef9ca6bda06072fcc62429579eb2b3d0c6459b
zewdu erkyhun <zewdu444@gmail.com>
2023-05-24 18:52:10 +0300

    Add test case for recipe request

--- spec/requests/recipe_spec.rb
+++ spec/requests/recipe_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe "Recipes", type: :request do
     @user = User.create(name: "Test user", email:"test444@gmail.com", password:"123456", password_confirmation:"123456", confirmation_token: nil, confirmed_at: Time.now)
     @recipe = Recipe.create(name: "Test recipe", preparation_time: 10.2, cooking_time: 20.3, description: "Test description", public: true, user_id: @user.id)
     @recipeTwo = Recipe.create(name: "Test recipe Two", preparation_time: 10.2, cooking_time: 20.3, description: "Test description two", public: true, user_id: @user.id)
-  end
-
+    post user_session_path params: {user: {email: @user.email, password: @user.password}}
+   end
   describe "GET /public recipe" do
     it "returns http success" do
        get public_recipes_path
@@ -20,5 +20,48 @@ RSpec.describe "Recipes", type: :request do
       get public_recipes_path
       expect(response.body).to include(@user.recipes.first.name)
    end
+  end
+  describe  "GET /recipes/" do
+      it "returns http success" do
+          get recipes_path
+        expect(response).to have_http_status(200)
+      end
+      it "render the index template" do
+          get recipes_path
+        expect(response).to render_template("index").or(render_template("recipes/index"))
+      end
+      it "displays the recipe name" do
+        get recipes_path
+        expect(response.body).to include(@user.recipes.first.name)
+      end
+  end
+  describe  "GET /recipes/new" do
+     it "returns http success" do
+          get new_recipe_path
+          expect(response).to have_http_status(200)
+     end
+     it "renders the new template" do
+          get new_recipe_path
+          expect(response).to render_template("new").or(render_template("recipes/new"))
+     end
+      it "displays the new recipe form" do
+          get new_recipe_path
+          expect(response.body).to include("Add New Recipe")
+      end
+  end
+  describe  "GET /recipes/:id" do
+      it "returns http success" do
+          get recipe_path(@recipe)
+          expect(response).to have_http_status(200)
+      end
+      it "renders the show template" do
+          get recipe_path(@recipe)
+          expect(response).to render_template("show").or(render_template("recipes/show"))
+      end
+      it "displays the recipe name" do
+          get recipe_path(@recipe)
+          expect(response.body).to include(@recipe.name)
+      end
+
   end
 end
