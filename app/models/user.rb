b854e33524dbdab26cb371360353b8e89e99ccd3
mrprotocoll <lekanvgbg@gmail.com>
2023-05-24 17:17:09 +0100

    Add total item and total price to public recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,28 +6,4 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   validates :name, presence: true
   has_many :foods, dependent: :destroy
-
-  def shopping_lists
-    recipes = self.recipes
-    general_food_list = []
-    recipes.each do |recipe|
-      recipe.recipe_foods.each do |recipe_food|
-        general_food_list << recipe_food.food
-      end
-    end
-    
-    user_food_list = self.foods
-    
-    missing_food_list = general_food_list - user_food_list
-    total_food_items = missing_food_list.size
-    total_price = missing_food_list.sum(&:price)
-    
-    {
-      missing_food_list: missing_food_list,
-      total_food_items: total_food_items,
-      total_price: total_price
-    }
-
-  end
-
 end
