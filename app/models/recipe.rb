9982cd06fa398a4c4d9f03ac0a04a11d0d2f4590
zewdu erkyhun <zewdu444@gmail.com>
2023-05-24 15:22:04 +0300

    Add test case for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,6 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :preparation_time, presence: true, numericality: { greater_than: 0 }
   validates :cooking_time, presence: true, numericality: { greater_than: 0 }
+  validates :description, presence: true
   has_many :recipe_foods, dependent: :destroy
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -8,5 +8,37 @@ RSpec.describe Recipe, type: :model do
   it "is valid with valid attributes" do
     expect(@recipe).to be_valid
   end
+  it "is not valid without a name" do
+    @recipe.name = nil
+    expect(@recipe).to_not be_valid
+  end
+  it "is not valid without a preparation time" do
+    @recipe.preparation_time = nil
+    expect(@recipe).to_not be_valid
+  end
+  it "is not valid without a cooking time" do
+    @recipe.cooking_time = nil
+    expect(@recipe).to_not be_valid
+  end
+  it "is not valid without a description" do
+    @recipe.description = nil
+    expect(@recipe).to_not be_valid
+  end
+  it "is not valid with a preparation time less than 0" do
+    @recipe.preparation_time = -1
+    expect(@recipe).to_not be_valid
+  end
+  it "is not valid with a cooking time less than 0" do
+    @recipe.cooking_time = -1
+    expect(@recipe).to_not be_valid
+  end
+  it "is not valid with a preparation time equal to 0" do
+    @recipe.preparation_time = 0
+    expect(@recipe).to_not be_valid
+  end
+  it "is not valid with a cooking time equal to 0" do
+    @recipe.cooking_time = 0
+    expect(@recipe).to_not be_valid
+  end
 
 end
